e50f7ac966f0f7659f5eaea6f08a3cfd79e904b6
Jason <348842258@qq.com>
2020-03-30 16:23:32 +0800

    first commit

---
+++ src/test/java/com/scloud/config/ConfigApplicationTests.java
@@ -0,0 +1,11 @@
+package com.scloud.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
